ed521b54cfeda128cd55985a7f1c221bebc46384
ZL&EL <lzc123456ab@gmail.com>
2023-09-17 20:08:40 -0400

    1

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-document.querySelector("button").addEventListener("click", handleClick);  // if use handleClick(), it will call the function once the listener is added, it will return the function, the return is undefined. handleClick is only run when you click.
-function handleClick(){
+
+for(let i = 0; i < document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
     alert("i got");
+}); 
 }
+
+
+ // if use handleClick(), it will call the function once the listener is added, it will return the function, the return is undefined. handleClick is only run when you click.
+// function handleClick(){
+//     alert("i got");
+// }
